99352150043e125a7b4f1b22e8cd7dbb15912c0f
Tyler Kindy <tylerkindy@gmail.com>
2023-02-12 21:32:55 -0500

    Extract template namespace

--- src/com/tylerkindy/website/home.clj
+++ src/com/tylerkindy/website/home.clj
@@ -1,45 +1,9 @@
 (ns com.tylerkindy.website.home
-  (:require [hiccup.page :refer [html5]]))
-
-(def nav-items [{:name "Home", :link "/"}
-                {:name "Blog", :link "/blog"}])
-(def social-links [{:name "GitHub"
-                    :image "github.png"
-                    :link "https://github.com/tkindy"}
-                   {:name "Twitter"
-                    :image "twitter.svg"
-                    :link "https://twitter.com/TylerKindy"}
-                   {:name "Mastodon"
-                    :image "mastodon.svg"
-                    :link "https://mastodon.social/@TylerKindy"
-                    :rel :me}])
-
-(defn nav []
-  [:nav
-   [:h1.name "Tyler Kindy"]
-   [:div.pages
-    (for [{:keys [link name]} nav-items]
-      [:a {:href link} name])]
-   [:div.social-links
-    (for [{:keys [rel link image name]} social-links]
-      [:a {:rel rel, :href link}
-       [:img {:src (str "/images/" image)
-              :alt name}]])]])
-
-(defn template [content]
-  (list
-   [:head
-    [:meta {:charset :utf-8}]
-    [:meta {:name :viewport, :content "width=device-width, initial-scale=1"}]
-    [:link {:rel :stylesheet, :href "/css/main.css"}]
-    [:link {:rel :icon, :href "/favicon.ico", :type "image/x-icon"}]]
-   [:body
-    [:div.container
-     (nav)
-     content]]))
+  (:require [com.tylerkindy.website.templates :refer [default]]
+            [hiccup.page :refer [html5]]))
 
 (defn home []
-  (template
+  (default
    [:div.home-container
     [:img.me-img {:src "/images/tyler.jpg", :alt "Tyler"}]
     [:div.me-description
---
+++ src/com/tylerkindy/website/templates.clj
@@ -0,0 +1,38 @@
+(ns com.tylerkindy.website.templates)
+
+(def nav-items [{:name "Home", :link "/"}
+                {:name "Blog", :link "/blog"}])
+(def social-links [{:name "GitHub"
+                    :image "github.png"
+                    :link "https://github.com/tkindy"}
+                   {:name "Twitter"
+                    :image "twitter.svg"
+                    :link "https://twitter.com/TylerKindy"}
+                   {:name "Mastodon"
+                    :image "mastodon.svg"
+                    :link "https://mastodon.social/@TylerKindy"
+                    :rel :me}])
+
+(defn nav []
+  [:nav
+   [:h1.name "Tyler Kindy"]
+   [:div.pages
+    (for [{:keys [link name]} nav-items]
+      [:a {:href link} name])]
+   [:div.social-links
+    (for [{:keys [rel link image name]} social-links]
+      [:a {:rel rel, :href link}
+       [:img {:src (str "/images/" image)
+              :alt name}]])]])
+
+(defn default [content]
+  (list
+   [:head
+    [:meta {:charset :utf-8}]
+    [:meta {:name :viewport, :content "width=device-width, initial-scale=1"}]
+    [:link {:rel :stylesheet, :href "/css/main.css"}]
+    [:link {:rel :icon, :href "/favicon.ico", :type "image/x-icon"}]]
+   [:body
+    [:div.container
+     (nav)
+     content]]))
