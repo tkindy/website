c1dc81a9d059d42a8230ec60fabea5b52eb0facc
Tyler Kindy <tylerkindy@gmail.com>
2023-02-12 22:48:25 -0500

    Make intermediate output directories

--- src/com/tylerkindy/website/output.clj
+++ src/com/tylerkindy/website/output.clj
@@ -1,11 +1,17 @@
 (ns com.tylerkindy.website.output
-  (:require [hiccup.page :refer [html5]]))
+  (:require [hiccup.page :refer [html5]])
+  (:import [java.nio.file Path]))
 
 (def output-dir "output")
+(def output-path (Path/of output-dir (make-array String 0)))
 
 (defn page [contents]
   (html5 {:lang :en} contents))
 
 (defn output [path contents]
-  (spit (str output-dir "/" path)
-        contents))
+  (let [path (.resolve output-path path)]
+    (-> path
+        .getParent
+        .toFile
+        .mkdirs)
+    (spit (.toFile path) contents)))
