441d83a73c5f1a989c14b695363e17e6179569f3
Tyler Kindy <tylerkindy@gmail.com>
2023-02-18 22:56:28 -0500

    Port image include

--- src/com/tylerkindy/website/mdj.clj
+++ src/com/tylerkindy/website/mdj.clj
@@ -1,6 +1,15 @@
 (ns com.tylerkindy.website.mdj
   (:require [clojure.string :as str]
-            [commonmark-hiccup.core :refer [markdown->hiccup]]))
+            [commonmark-hiccup.core :refer [markdown->hiccup]]
+            [hiccup.core :refer [html]]))
+
+(defn image [{:keys [file alt class caption]}]
+  (html
+   [:figure.centered
+    [:img {:src (str "/images/" file)
+           :alt alt
+           :class class}]
+    [:figcaption caption]]))
 
 (defn mdj->markdown [mdj]
   (str/replace mdj
