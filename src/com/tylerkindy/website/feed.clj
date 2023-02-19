c1e71683b22b11e606547ccf2e08a186b1e79ced
Tyler Kindy <tylerkindy@gmail.com>
2023-02-19 00:41:48 -0500

    Fill out feed metadata

--- src/com/tylerkindy/website/feed.clj
+++ src/com/tylerkindy/website/feed.clj
@@ -1,11 +1,26 @@
 (ns com.tylerkindy.website.feed
   (:require [clojure.data.xml :as xml]
-            [com.tylerkindy.website.output :refer [output]]))
+            [com.tylerkindy.website.output :refer [output]]
+            [com.tylerkindy.website.url :refer [absolute-url]]))
+
+(def feed-path "feed.xml")
 
 (defn feed [posts]
   (xml/sexp-as-element
-   [:feed {:xmlns "http://www.w3.org/2005/Atom"}]))
+   [:feed {:xmlns "http://www.w3.org/2005/Atom"}
+    [:generator {:uri "https://github.com/tkindy/website"}
+     "Tyler Kindy's static site generator"]
+    [:link {:href (absolute-url (str "/" feed-path))
+            :rel "self"
+            :type "application/atom+xml"}]
+    [:link {:href (absolute-url "/")
+            :rel "alternate"
+            :type "text/html"}]
+    [:updated (-> (java.time.Instant/now)
+                  (.truncatedTo java.time.temporal.ChronoUnit/SECONDS))]
+    [:id (absolute-url (str "/" feed-path))]
+    [:title {:type "html"} "Tyler Kindy"]]))
 
 (defn build-feed [posts]
-  (output "feed.xml"
+  (output feed-path
           (xml/emit-str (feed posts))))
