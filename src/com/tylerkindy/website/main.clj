ccd842e7561805e6cd79fab3483fd10a8994c2df
Tyler Kindy <tylerkindy@gmail.com>
2026-06-02 22:21:40 -0400

    Add example output

--- src/com/tylerkindy/website/main.clj
+++ src/com/tylerkindy/website/main.clj
@@ -1,4 +1,13 @@
-(ns com.tylerkindy.website.main)
+(ns com.tylerkindy.website.main
+  (:require [babashka.fs :as fs]
+            [com.tylerkindy.website.paths :refer [out-dir]]
+            [hiccup.page :refer [html5]]))
 
 (defn -main []
-  (println "Hello, World!"))
+  (fs/create-dirs out-dir)
+  (->> (fs/glob out-dir "*")
+       (map fs/delete-tree)
+       doall)
+  (spit (fs/file (fs/path out-dir "index.html"))
+        (html5 [:body
+                [:h1 "Tyler Kindy"]])))
