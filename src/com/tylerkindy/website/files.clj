8fd697ca13f220a809ca8cfed49e92ff54218fe4
Tyler Kindy <tylerkindy@gmail.com>
2026-06-04 22:01:00 -0400

    Extract files

---
+++ src/com/tylerkindy/website/files.clj
@@ -0,0 +1,21 @@
+(ns com.tylerkindy.website.files
+  (:require [com.tylerkindy.website.css :as css]
+            [hiccup.page :refer [html5]]))
+
+(defn page [content]
+  (html5 [:head
+          [:link {:rel :stylesheet
+                  :href "/css/main.css"}]]
+         [:body
+          [:div
+           [:nav
+            [:a {:href "/"} "Tyler Kindy"]
+            [:a {:href "/blog"} "Blog"]]
+           [:main content]]]))
+
+(defn home []
+  (page [:p "Here's some content"]))
+
+(def files
+  {"index.html" home
+   "css/main.css" css/main})
--- src/com/tylerkindy/website/generate.clj
+++ src/com/tylerkindy/website/generate.clj
@@ -1,36 +1,19 @@
 (ns com.tylerkindy.website.generate
   (:require [babashka.fs :as fs]
-            [com.tylerkindy.website.css :as css]
-            [com.tylerkindy.website.paths :refer [assets-dir out-dir]]
-            [hiccup.page :refer [html5]]))
-
-(defn page [content]
-  (html5 [:head
-          [:link {:rel :stylesheet
-                  :href "/css/main.css"}]]
-         [:body
-          [:div
-           [:nav
-            [:a {:href "/"} "Tyler Kindy"]
-            [:a {:href "/blog"} "Blog"]]
-           [:main content]]]))
-
-(defn home []
-  (page [:p "Here's some content"]))
+            [com.tylerkindy.website.files :refer [files]]
+            [com.tylerkindy.website.paths :refer [assets-dir out-dir]]))
 
 (defn generate [_]
   (fs/create-dirs out-dir)
   (->> (fs/list-dir out-dir)
        (map fs/delete-tree)
        doall)
-  (let [files {"index.html" (home)
-               "css/main.css" (css/main)}]
-    (doall (for [[path content] files]
-             (do
-               (some->> path
-                        fs/parent
-                        (fs/path out-dir)
-                        fs/create-dirs)
-               (spit (fs/file (fs/path out-dir path))
-                     content)))))
+  (doall (for [[path content] files]
+           (do
+             (some->> path
+                      fs/parent
+                      (fs/path out-dir)
+                      fs/create-dirs)
+             (spit (fs/file (fs/path out-dir path))
+                   (content)))))
   (fs/copy-tree assets-dir (fs/path out-dir "assets")))
